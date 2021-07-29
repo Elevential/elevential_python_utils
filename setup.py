7de40412aa7dc799ee5e3ba37a82f8faf1d8e2a6
moutella <joaogabrielmsc@gmail.com>
2021-07-29 13:26:49 -0300

    changed lib name

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 print(find_packages())
 setup(
-    name = "smart_lib",
-    version = "0.1",
+    name = "elevential",
+    version = "1",
     packages=find_packages(),
     install_requires=[]
 )
